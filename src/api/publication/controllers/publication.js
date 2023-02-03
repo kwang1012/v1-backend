f0d60db2a7e91f84c955caad6472b1f76499da70
kswang <kswang@lsalab.cs.nthu.edu.tw>
2023-02-03 10:58:42 +0000

    add group parameters in publictions

--- src/api/publication/controllers/publication.js
+++ src/api/publication/controllers/publication.js
@@ -1,9 +1,40 @@
-'use strict';
+"use strict";
 
 /**
  * publication controller
  */
 
-const { createCoreController } = require('@strapi/strapi').factories;
+const { createCoreController } = require("@strapi/strapi").factories;
+const _ = require("lodash");
 
-module.exports = createCoreController('api::publication.publication');
+const camelize = (obj) =>
+  _.transform(obj, (acc, value, key, target) => {
+    const camelKey = _.isArray(target) ? key : _.camelCase(key);
+    acc[camelKey] = _.isDate(value)
+      ? value.toISOString()
+      : _.isObject(value)
+      ? camelize(value)
+      : value;
+  });
+
+module.exports = createCoreController(
+  "api::publication.publication",
+  ({ strapi }) => ({
+    async find(ctx) {
+      if (ctx.query.group) {
+        const knex = strapi.db.connection;
+        const results = await knex("publications")
+          .select(
+            knex.raw(
+              `json_agg(publications.*) as pubs, date_trunc('${ctx.query.group}', date) as year`
+            )
+          )
+          .groupBy("year")
+          .orderBy("year", "desc");
+        return this.transformResponse(camelize(results));
+      } else {
+        return super.find(ctx);
+      }
+    },
+  })
+);
